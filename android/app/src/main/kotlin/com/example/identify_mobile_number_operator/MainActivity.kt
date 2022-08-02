843dc7d6750d6125fb7a034bf2e0f0bcdf74f088
Emeka212 <stephenugo939@gmail.com>
2022-08-02 11:06:14 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/identify_mobile_number_operator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.identify_mobile_number_operator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:identify_mobile_number_operator/index.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
